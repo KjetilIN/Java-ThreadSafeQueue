dfa33920c041c2d165d86623bb27d61bd8ff7544
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-09-02 15:41:25 +0200

    add: working main code that is deterministic

--- src/no/uio/ifi/task/main.java
+++ src/no/uio/ifi/task/main.java
@@ -4,10 +4,14 @@ import java.util.HashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /* You are allowed to 1. add modifiers to fields and method signatures of subclasses, and 2. add code at the marked places, including removing the following return */
 public class main {
     public static void main(String[] args) throws InterruptedException {
 
+        /** Linked Queues */
         LinkedQueue<Integer> inputQueue = new LinkedQueue<>();
         LinkedQueue<Integer> evenQueue = new LinkedQueue<>();
         LinkedQueue<Integer> oddQueue = new LinkedQueue<>();
@@ -18,21 +22,30 @@ public class main {
 
         int n = 1000;
 
+        /** Insert to queue service
+         * The API for the thread pool allows us to submit a new task. 
+         * In this case, we start a new task for inserting numbers to the queue. 
+         */
         ExecutorService inputExc = Executors.newCachedThreadPool();
         // Starting threads to insert numbers into the input queue
         for (int i = 1; i <= n; i++){
             // Submit the task of inserting the number to the thread pool
+            // Each task is to submit a number to the start queue 
             final int numberToInsert = i; 
             inputExc.submit(() ->{
                 inputQueue.insert(numberToInsert);
             });
         }
+        // Here we call the shutdown method. It tells the pool, that no more task will be entered to the thread pool
         inputExc.shutdown();
 
+        // The two mappers that are each responsible to take from the input queue 
+        // Both mapper will both insert to both queues 
         Mapper<Integer, Boolean> mapper1 = new Mapper<Integer, Boolean>(layer) {
             @Override
             void transform(Integer input) {
-                // take number and put it into the right queue 
+                // Take number and put it into the right queue 
+                // We get the queue we want to add data to and then insert the input squared
                 LinkedQueue<Integer> targetQueue = layer.get(input % 2 == 0); 
                 targetQueue.insert(input * input); 
                 this.count += 1;
@@ -41,31 +54,35 @@ public class main {
         Mapper<Integer, Boolean> mapper2 = new Mapper<Integer, Boolean>(layer) {
             @Override
             void transform(Integer input) {
-                // take number and put it into the right queue  
+                // Same implementation as mapper1
                 LinkedQueue<Integer> targetQueue = layer.get(input % 2 == 0); 
                 targetQueue.insert(input * input); 
                 this.count += 1;
             }
         };
 
+        // Creating the pool of threads to distribute 
+        // Start N tasks that each take from the input queue 
         ExecutorService distribute = Executors.newCachedThreadPool();
-        /* TODO: start n threads, each taking a single number from inputQueue to either mapper1 or mapper2
-        *        each mapper must have the same amount of work
-        *        the mapper must add its number to the correct queue*/
 
-        // Shared atomic counter for distributing tasks
+        /** Shared atomic counter for distributing tasks
+         * This is an object part of the java.util.concurrent.atomic library.
+         * It acts as a integer, but all operations are atomic. 
+         * This will act as a way to distribute the workload between the two mappers
+         *  - distribute based on if the integer is even or odd? 
+        */ 
         AtomicInteger distributeCounter = new AtomicInteger(0);
-
         // Starting threads distribute the work to headers 
         for (int i = 1; i <= n; i++){
             distribute.submit(() ->{
                 // Try to get the number from delfront method
-                // Remove the number from the queue
-                // If number is null, then we know the queue 
+                // Remove the number from the queue with the delfront() queue 
+                // If number is null, then we know the queue is empty.
                 Integer number = inputQueue.delfront();
                 if (number != null){
                     // Atomic fetch and increment 
                     // Then based on if the number is odd or even, distribute the work to a mapper
+                    // We get and increment atomically to ensure that we get either an odd or even number
                     if (distributeCounter.getAndIncrement() % 2 == 0){
                         mapper1.transform(number);
                     }else{
@@ -74,38 +91,76 @@ public class main {
                 }                
             });
         }
+
+        // Shutdown, because no more numbers to distribute  
         distribute.shutdown();
 
+
+        /** Reducers:
+         * We assign one reducer for each queue - even and odd numbers.
+         * The process itself is very simple, but we still need a lock for each. 
+         * This is because there are some cases where the operations of incrementing the count and current leads to non deterministic result.
+         * The original code result: 
+         * Lock around 
+         * InputQueue count: 1000
+         * OddQueue count: 500
+         * EvenQueue count: 500
+         * Mapper1 count: 500
+         * Mapper2 count: 500
+         * Sum even: 165458664
+         * Count even: 497 => Suggests that count happened in a way that lead to a bad result. And 
+         * Sum odd: 165791482
+         * Count odd: 498
+         * 2583354
+         * 
+         * With the lock for each reducer, we assure that we get a more deterministic result.
+         */
+        Lock reduceLock1 = new ReentrantLock();
         Reducer<Integer> reducer1 = new Reducer<Integer>() {
             @Override
             protected void reduce(Integer input) {
-                this.count += 1; 
-                this.current += input;
-
+                reduceLock1.lock();
+                try{
+                    this.count += 1; 
+                    this.current += input;
+                }finally{
+                    reduceLock1.unlock();
+                }
             }
         };
+
+        Lock reduceLock2 = new ReentrantLock();
         Reducer<Integer> reducer2 = new Reducer<Integer>() {
             @Override
             protected void reduce(Integer input) {
-                this.count += 1; 
-                this.current += input;
+                reduceLock2.lock();
+                try{
+                    this.count += 1; 
+                    this.current += input;
+                }finally{
+                    reduceLock2.unlock();
+                }
             }
         };
 
 
+        // Reducer thread-pool 
         ExecutorService reduce = Executors.newCachedThreadPool();
-
-        /* TODO: start n threads, each taking one number from either queue and giving it to a reducer.
-        *        Reducer 1 will only add even numbers, reducer 2 will only add off numbers */
-
         for (int j = 1; j<=n; j++){
+            // Reduce task to be submitted
             final int reduceNumber = j; 
             reduce.submit(()->{
+                // Boolean for what queue to take from 
+                // Will evenly distribute the reduce method for each 
+                // This is also another way to solve the task of distributing 
                 boolean isEven = reduceNumber % 2 == 0;
+
+                // Get a queue based on if the even or odd task number. 
                 LinkedQueue<Integer> targetQueue = layer.get(isEven); 
                 Integer numberFromQueue = targetQueue.delfront();
 
-                if (reduceNumber % 2 == 0){
+                // Distribute and then reduce 
+                if (isEven){
                     reducer1.reduce(numberFromQueue);
                 }else{
                     reducer2.reduce(numberFromQueue);
@@ -113,21 +168,14 @@ public class main {
                             
             });
         }
+
+        // Shutdown reducer thread pool. No more tasks. 
         reduce.shutdown();
 
+        // Final tests
         Thread.sleep(2000);
-
-        System.out.println("InputQueue count: " + inputQueue.count);
-        System.out.println("OddQueue count: " + oddQueue.count);
-        System.out.println("EvenQueue count: " + evenQueue.count);
-
-        System.out.println("Mapper1 count: " + mapper1.count);
-        System.out.println("Mapper2 count: " + mapper2.count);
-
         System.out.println("Sum even: "+reducer1.current);
-        System.out.println("Count even: " + reducer1.count);
         System.out.println("Sum odd: "+reducer2.current);
-        System.out.println("Count odd: " + reducer2.count);
 
         int total = 0;
         for(int i = 1; i<= n; i++){
